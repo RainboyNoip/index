7a16a134b55266a0ce7b14a7e09abf14e3359db9
rainboy <rainboylvx@qq.com>
2019-06-02 10:10:01 +0800

    update

--- sites.js
+++ sites.js
@@ -1,8 +1,9 @@
 var sites = [
-    { logo:"vim.png", name:"vim配置",href:"#"},
-    { logo:"vim.png", name:"vim配置",href:"#"},
-    { logo:"vim.png", name:"vim配置",href:"#"},
-    { logo:"vim.png", name:"vim配置",href:"#"},
-    { logo:"vim.png", name:"vim配置",href:"#"},
+    { logo:"vim.png", name:"vim配置",href:"https://rainboy.gitee.io/sample-vim-config/"},
+    { logo:"slime.png", name:"slimeOJ",href:"#"},
+    { logo:"book.png", name:"R-Book",href:"rbook.noier.fun"},
+    { logo:"flow.png", name:"DSA",href:"dsa.slimeoj.online"},
+    { logo:"blog.png", name:"作者博客",href:"rainboy."},
+    { logo:"about.png", name:"关于作者",href:"rainboy."},
 ]
 
