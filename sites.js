7cc26a4025c30656d990867d10b2d3c79388382d
rainboy <rainboylvx@qq.com>
2019-10-31 17:06:19 +0800

    update

--- sites.js
+++ sites.js
@@ -3,7 +3,7 @@ var sites = [
     { logo:"slime.png", name:"slimeOJ",href:"http://moonoj.iok.la"},
     { logo:"rbook.ico", name:"R-Book",href:"https://rbook.slimeoj.online"},
     { logo:"dsa.ico", name:"DSA",href:"http://rainboy.coding.me/Data-Structure-Animation/"},
-    { logo:"flow.png", name:"OLD-DSA",href:"dsa.slimeoj.online"},
+    { logo:"flow.png", name:"OLD-DSA",href:"http://dsa.slimeoj.online"},
     { logo:"blog.png", name:"作者博客",href:"https://blog.slimeoj.online"},
     { logo:"about.png", name:"关于作者",href:"https://rainboy.gitee.io/resume/"},
 ]
