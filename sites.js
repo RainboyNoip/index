51295a515ee359e747a50e879c9bd46056a4bdbd
rainboy <rainboylvx@qq.com>
2019-10-31 17:42:30 +0800

    update

--- sites.js
+++ sites.js
@@ -4,6 +4,7 @@ var sites = [
     { logo:"rbook.ico", name:"R-Book",href:"https://rbook.slimeoj.online"},
     { logo:"dsa.ico", name:"DSA",href:"http://rainboy.coding.me/Data-Structure-Animation/"},
     { logo:"flow.png", name:"OLD-DSA",href:"http://dsa.slimeoj.online"},
+    { logo:"snippets.ico", name:"rSnip",href:"https://github.com/Rainboylvx/rSnip"},
     { logo:"blog.png", name:"作者博客",href:"https://blog.slimeoj.online"},
     { logo:"about.png", name:"关于作者",href:"https://rainboy.gitee.io/resume/"},
 ]
